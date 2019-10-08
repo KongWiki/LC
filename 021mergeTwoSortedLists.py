9d203be87ae0d35cbacaf4ea9d251f6a57c0468c
KongWiki <kongwiki@163.com>
2019-10-08 21:37:22 +0800

    添加 021 merge two sorted lists

--- 021mergeTwoSortedLists.py
+++ 021mergeTwoSortedLists.py
@@ -1,25 +1,64 @@
 """
-@author  : kongweikun
-@file    : 021mergeTwoSortedLists.py
-@time    : 18-8-15 下午12:43
-@contact : kongwiki@163.com
+@time : 2019/10/8下午8:11
+@Author: kongwiki
+@File: 021mergeTwoSortedLists.py
+@Email: kongwiki@163.com
 """
-import re
+"""
+Merge two sorted linked lists and return it as a new list. 
+The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+
+"""
 
 
-def mergeTwoLists(l1, l2):
-	"""
-	:type l1:
-	:type l2:
-	:type:
-	"""
-	for i in l2:
-		l1.append(i)
-	l1 = sorted(l1)
-	print(l1)
+class ListNode:
+	def __init__(self, x):
+		self.val = x
+		self.next = None
+
+	def myprint(self):
+		print(self.val)
+		if self.next:
+			self.next.myprint()
+
+
+class Solution:
+	def mergeTwoLists(self, l1, l2):
+		if not l1 or not l2:
+			return l1 or l2
+		head = cur = ListNode(0)
+		while l1 and l2:
+			if l1.val < l2.val:
+				cur.next = l1
+				l1 = l1.next
+			else:
+				cur.next = l2
+				l2 = l2.next
+			cur = cur.next
+		cur.next = l1 or l2
+		return head.next
 
 
 if __name__ == '__main__':
-	l1 = [1, 2, 4]
-	l2 = [1, 3, 4]
-	mergeTwoLists(l1, l2)
+	s = Solution()
+	n6 = ListNode(4)
+	n5 = ListNode(3)
+	n4 = ListNode(1)
+	n3 = ListNode(4)
+	n2 = ListNode(2)
+	n1 = ListNode(1)
+	n1.next = n2
+	n2.next = n3
+
+	n4.next = n5
+	n5.next = n6
+
+	# n1.myprint()
+	# print('\n')
+	# n4.myprint()
+	s.mergeTwoLists(n1, n4).myprint()
